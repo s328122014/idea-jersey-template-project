df3aa72fcadba08c66cd6b40b51cd75b4bf2cb9c
Amir Halperin <amiram.halperin@gmail.com>
2015-01-19 10:56:04 +0200

    Initial commit

---
+++ server/src/main/java/ah/myapp/server/PersonService.java
@@ -0,0 +1,28 @@
+package ah.myapp.server;
+
+import ah.myapp.server.dm.Person;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/person")
+public class PersonService
+{
+    @GET
+    public String getName()
+    {
+        return "my name";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response Create(Person p)
+    {
+        return Response.status(200).entity("Person ID " + p.Id + " Name " + p.Name + " created").build();
+    }
+}
+
---
+++ server/src/main/java/ah/myapp/server/dm/Person.java
@@ -0,0 +1,8 @@
+package ah.myapp.server.dm;
+
+public class Person
+{
+    public String Name;
+    public int Id;
+}
+
