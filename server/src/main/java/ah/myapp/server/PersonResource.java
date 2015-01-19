96ed0498e4df93fcf914d1a9f28fb68f24e8c2c4
Amir Halperin <amiram.halperin@gmail.com>
2015-01-19 12:44:17 +0200

    refactoring

--- server/src/main/java/ah/myapp/server/PersonResource.java
+++ server/src/main/java/ah/myapp/server/PersonResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/person")
-public class PersonService
+public class PersonResource
 {
     @GET
     public String getName()
